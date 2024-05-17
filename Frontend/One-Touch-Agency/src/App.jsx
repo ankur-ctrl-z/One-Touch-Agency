53121b17fe72aeb2c65bca3719fd659c072a2899
Ankur Sharma <113872942+ankur1435@users.noreply.github.com>
2024-05-17 21:29:59 +0530

    added footer

--- Frontend/One-Touch-Agency/src/App.jsx
+++ Frontend/One-Touch-Agency/src/App.jsx
@@ -2,14 +2,16 @@ import NavBar from './components/Navbar/NavBar'
 import Video from "./components/Video/Video"
 import Services from './components/Services/Services'
 import ContactForm from './components/ContactForm/ContactForm'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <div>
-      <NavBar></NavBar>
-      <Video></Video>
-      <Services></Services>
-      <ContactForm></ContactForm>
+      {/* <NavBar></NavBar> */}
+      {/* <Video></Video> */}
+      {/* <Services></Services> */}
+      {/* <ContactForm></ContactForm> */}
+      <Footer></Footer>
     </div>
   )
 }
--- Frontend/One-Touch-Agency/src/components/ContactForm/ContactForm.jsx
+++ Frontend/One-Touch-Agency/src/components/ContactForm/ContactForm.jsx
@@ -1,24 +1,37 @@
 const ContactForm = () => {
   return (
     <section className="text-gray-600 body-font relative">
-      <div className="absolute inset-0 bg-gray-300">
-        <iframe
-          width="100%"
-          height="100%"
-          frameBorder="0"
-          marginHeight="0"
-          marginWidth="0"
-          title="map"
-          scrolling="no"
-          src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;q=%C4%B0zmir+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed"
-          style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }}
-        />
-      </div>
-      <div className="container px-5 py-24 mx-auto flex">
-        <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-          <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">GET IN TOUCH</h2>
+      <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+          <iframe
+            width="100%"
+            height="100%"
+            className="absolute inset-0"
+            frameBorder="0"
+            title="map"
+            marginHeight="0"
+            marginWidth="0"
+            scrolling="no"
+            src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+            style={{ filter: 'grayscale(1) contrast(1.2) opacity(0.4)' }}
+          ></iframe>
+          <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6">
+              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">ADDRESS</h2>
+              <p className="mt-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+            </div>
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">EMAIL</h2>
+              <a className="text-indigo-500 leading-relaxed">example@email.com</a>
+              <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">PHONE</h2>
+              <p className="leading-relaxed">123-456-7890</p>
+            </div>
+          </div>
+        </div>
+        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h1 className="text-gray-900 text-2xl mb-2 font-bold title-font">GET IN TOUCH</h1>
           <div className="relative mb-4">
-            <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+            <label htmlFor="email" className="leading-7 text-lg text-gray-600">Email</label>
             <input
               type="email"
               id="email"
@@ -27,16 +40,16 @@ const ContactForm = () => {
             />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="service" className="leading-7 text-sm text-gray-600">Service</label>
+            <label htmlFor="services" className="leading-7 text-lg text-gray-600">Services</label>
             <input
               type="text"
-              id="service"
-              name="service"
+              id="services"
+              name="services"
               className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
             />
           </div>
           <div className="relative mb-4">
-            <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
+            <label htmlFor="message" className="leading-7 text-lg text-gray-600">Messages</label>
             <textarea
               id="message"
               name="message"
@@ -51,6 +64,8 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
+
+
 
 
 
